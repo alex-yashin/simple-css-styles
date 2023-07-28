2418c6a1a217a57223cea867c1422f25f5dd9d5e
Alex Yashin <alex-yashin@ya.ru>
2023-07-28 15:04:05 +0400

    fix

--- src/form/textarea.js
+++ src/form/textarea.js
@@ -1,7 +1,9 @@
 let textareaElements = document.querySelectorAll('textarea.form-control');
 let textareaAutogrow = function (e) {
+    let computed = window.getComputedStyle(e);
+    let padding = parseInt(computed.paddingTop) + parseInt(computed.paddingBottom);
     e.style.height = "5px";
-    e.style.height = (e.scrollHeight) + "px";
+    e.style.height = (e.scrollHeight - padding) + "px";
 };
 for (let i = 0; i < textareaElements.length; i++) {
     textareaElements[i].addEventListener('input', function () {
