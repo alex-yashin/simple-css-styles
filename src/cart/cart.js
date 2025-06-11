f281e8d685d6b0e0cf99ae8585c444eb9ea00e3e
Alex Yashin <alex-yashin@ya.ru>
2025-06-11 21:30:44 +0400

    промежуточный вариант для новой структуры кнопки добавления в корзину

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -1,9 +1,23 @@
-pn.each(".control-quantity", (c) => {
-    pn.clickIn(c, '.simple.btn', () => {
-        c.children[0].classList.remove('active');
-        c.children[1].classList.add('active');
+pn.each('.control-quantity', (c) => {
+    let q = c.getAttribute('data-amount');
+    if (typeof q == 'undefined') {
+        q = 0;
+    }
+    let preview = pn.span('preview', pn.span('', c.innerText));
+
+    c.innerText = '';
+    c.append(preview);
+
+    let cx = pn.span('complex');
+    cx.append(pn.span('minus'));
+    cx.append(pn.span('amount', q));
+    cx.append(pn.create('span', 'plus'));
+    c.append(cx);
+
+    c.addEventListener('click', () => {
+        c.classList.add('detailed');
         c.dispatchEvent(new Event('plus'));
     });
-    pn.clickIn(c, '.complex.btn .plus', () => c.dispatchEvent(new Event('plus')));
-    pn.clickIn(c, '.complex.btn .minus', () => c.dispatchEvent(new Event('minus')));
+    pn.clickIn(c, '.plus', (e) => {c.dispatchEvent(new Event('plus')); e.stopPropagation();});
+    pn.clickIn(c, '.minus', (e) => {c.dispatchEvent(new Event('minus')); e.stopPropagation();});
 });
